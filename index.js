8029d1507f2541a0139f7115e50f3c9f8ec8920d
AytacSahin <haytacsahin@gmail.com>
2023-07-21 13:52:39 +0300

    mainpage & image added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const server = require('./api/server.js');
 const { PORT } = require('./config/index.js');
+const express = require('express');
 
 require('dotenv').config();
+
+server.use(express.static('documentation'));
 
 server.get('/', (req, res, next) => {
     res.send(`
     <div style="text-align: center;">
         <h1>Welcome to Twitter Clone Project</h1>
-        <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="Twitter Logo" />
+        <img src="/twitter-bird-symbols-png-logo-0.png" alt="Twitter Logo" style="width: 30%;" />
         <p> Aytaç Şahin | 2023 </p>
     </div>
 `)
