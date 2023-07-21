b3e76457303597d241728f1d716fc8f930b39527
AytacSahin <haytacsahin@gmail.com>
2023-07-21 13:19:49 +0300

    index.js main page updated

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const { PORT } = require('./config/index.js');
 require('dotenv').config();
 
 server.get('/', (req, res, next) => {
-    res.send("<h1>Welcome to S16 Backend Challenge</h2> <p>Aytaç Şahin | 2023</p>")
+    res.send(`
+    <div style="text-align: center;">
+        <h1>Welcome to Twitter Clone Project</h1>
+        <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="Twitter Logo" />
+        <p> Aytaç Şahin | 2023 </p>
+    </div>
+`)
 })
 
 server.listen(PORT, () => {
